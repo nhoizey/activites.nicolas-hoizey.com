86ad9ab127294694fb66d160fb0c85a7f8891418
Nicolas Hoizey <nicolas@hoizey.com>
2025-07-07 16:57:33 +0200

    Clean useless content

--- src/assets/js/service-worker-install.js
+++
@@ -1,6 +0,0 @@
-// Install the Service Worker
-window.addEventListener("load", () => {
-	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker.register("/service-worker.js");
-	}
-});
--- workbox.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	globDirectory: "_site",
-	globPatterns: ["./bundle/*", "./manifest.webmanifest"],
-	swSrc: "_site/service-worker.js",
-	swDest: "_site/service-worker.js",
-};
