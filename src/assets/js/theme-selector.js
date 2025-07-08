f6b6327131778598c9806679448581eb3d228e91
Nicolas Hoizey <nicolas@hoizey.com>
2025-07-08 21:32:21 +0200

    Cleaner code

--- src/assets/js/theme-selector.js
+++ src/assets/js/theme-selector.js
@@ -1,25 +1,30 @@
-const themes = ['light', 'auto', 'dark'];
 const themeSelector = document.querySelector('#theme-selector');
 
 if (themeSelector) {
   // Get the range input element
   const rangeInput = themeSelector.querySelector('input[type="range"]');
 
+  // Get the list of theme items
+  const themeItems = themeSelector.querySelectorAll('.theme-labels li');
+
+  // Get the list of theme names
+  const themes = [...themeItems].map((item) => item.className);
+
   // Set the initial value based on localStorage or default to 'auto'
   rangeInput.value = themes.indexOf(localStorage.getItem('theme') || 'auto');
-
-  // Add event listeners to the labels
-  themeSelector.querySelectorAll('.theme-labels li').forEach((label, index) => {
-    label.addEventListener('click', () => {
-      rangeInput.value = index;
-      rangeInput.dispatchEvent(new Event('input'));
-    });
-  });
 
   // Update the theme when the range input changes
   rangeInput.addEventListener('input', (event) => {
     const newTheme = themes[event.target.value];
     document.querySelector('html').dataset.theme = newTheme;
     localStorage.setItem('theme', newTheme);
+  });
+
+  // Add event listeners to the labels
+  themeItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      rangeInput.value = index;
+      rangeInput.dispatchEvent(new Event('input'));
+    });
   });
 }
