ba7b142fc764e3720f8e58b25583df91ac866d33
Nicolas Hoizey <nicolas@hoizey.com>
2025-07-18 15:02:13 +0200

    Script pour créer facilement une nouvelle activité

---
+++ _scripts/activity.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+import fs from 'node:fs';
+import path from 'node:path';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { sharedSlugify } from '../node_modules/eleventy-plugin-pack11ty/_11ty/utils/slugify.js';
+
+const argv = yargs(hideBin(process.argv))
+  .locale('en')
+  .usage('Usage: $0 <command> [options]')
+  .command('* <title> [date]', 'add an activity', (yargs) => {
+    yargs
+      .positional('title', {
+        describe: 'Title of the activity',
+        type: 'string',
+        demandOption: true,
+      })
+      .positional('date', {
+        describe: 'Date of the activity',
+        type: 'string',
+        default: new Date().toISOString().split('T')[0]
+      })
+  })
+  .example('$0 "activity title" 2025-07-18', 'create a new activity with title and date')
+  .version('1.0')
+  .parse();
+
+const slug = sharedSlugify(argv.title);
+const folder = path.join('./src/collections/activites/', argv.date.replaceAll('-', '/'), slug);
+const content = `---
+title: ${argv.title}
+date: ${argv.date}
+type:
+duration:
+distance:
+tags: []
+strava:
+komoot:
+---
+
+`;
+
+if (fs.existsSync(folder)) {
+  console.error(`Activity folder already exists: ${folder}`);
+  process.exit(1);
+}
+
+['', 'photos', 'sources'].forEach(subfolder => {
+  const subfolderPath = path.join(folder, subfolder);
+  if (!fs.existsSync(subfolderPath)) {
+    fs.mkdirSync(subfolderPath, { recursive: true });
+  }
+});
+
+fs.writeFileSync(path.join(folder, "index.md"), content, 'utf8');
+console.log(`Activity created at: ${folder}`);
