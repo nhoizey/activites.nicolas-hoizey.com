6ea6fe1d6c66eefa55e07c98754e4281e0eccc2f
Nicolas Hoizey <nicolas@hoizey.com>
2025-08-04 14:57:26 +0200

    Réduire la taille des vignettes de photos sur la carte

--- src/assets/js/activite.js
+++ src/assets/js/activite.js
@@ -114,7 +114,7 @@ import { lineString, bbox, bearing, point } from "@turf/turf";
             // Create a DOM element for each marker.
             const el = document.createElement('div');
             el.className = 'marker';
-            el.style.backgroundImage = `url(${src})`;
+            el.style.backgroundImage = `url(${src.replace(/\/w_640\//, '/w_150/')})`;
 
             // Add the marker to the map
             new mapboxgl.Marker(el)
