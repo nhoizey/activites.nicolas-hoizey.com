963f3dc9d537c601e8ae286729165a2dec6d2a85
Nicolas Hoizey <nicolas@hoizey.com>
2025-08-13 21:39:25 +0200

    Implémentation plutôt avec une macro Nunjucks

--- src/_11ty/filters/collection.js
+++ src/_11ty/filters/collection.js
@@ -2,3 +2,10 @@ export const type = (collection, type) => {
   return collection
     .filter((activite) => activite.data.type === type);
 };
+
+export const withPathStem = (collection, pathStem) => {
+  return collection
+    .filter((activite) => {
+      return activite.page.filePathStem.replace(/^\/collections\/activites\/(.*)index$/, "$1") === pathStem;
+    })[0];
+}
