85e73b0d35ea67455a2e4451242ed1add0d624e6
Nicolas Hoizey <nicolas@hoizey.com>
2025-08-22 12:53:23 +0200

    Un global data file pour les couleurs

---
+++ src/_data/colors.js
@@ -0,0 +1,17 @@
+// Based on color palettes for data visualizations from IBM Carbon Design System
+// https://carbondesignsystem.com/data-visualization/color-palettes/
+
+export const colorsOnLight = ['#6929c4', '#1192e8', '#005d5d', '#9f1853', '#9f1853', '#570408', '#198038', '#002d9c', '#ee538b', '#b28600', '#009d9a', '#012749', '#8a3800', '#a56eff'];
+
+export const colorsOnDark = ['#8a3ffc', '#33b1ff', '#007d79', '#ff7eb6', '#fa4d56', '#fff1f1', '#6fdc8c', '#4589ff', '#d12771', '#d2a106', '#08bdba', '#bae6ff', '#ba4e00', '#d4bbff'];
+
+export const colorsByType = {
+  gravel: '#d12771',
+  vélo: '#ee80af',
+  VTT: '#de5590',
+  marche: '#08bdba',
+  tennis: '#8a3ffc',
+  padel: '#a36df4',
+  badminton: '#d4bbff',
+  'ski alpin': '#4589ff',
+};
