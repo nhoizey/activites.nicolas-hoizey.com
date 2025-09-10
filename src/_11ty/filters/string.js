de808e48d1dfc44ac4a3c8785f88350c8e86f76b
Nicolas Hoizey <nicolas@hoizey.com>
2025-09-10 14:12:53 +0200

    Ajout des flux de syndication Atom

--- src/_11ty/filters/string.js
+++ src/_11ty/filters/string.js
@@ -4,3 +4,23 @@ export const removeEmojis = (str) => {
 
   return str.replace(/(?!(\*|#|\d))[\p{Extended_Pictographic}\p{Emoji_Component}]|[\u0030-\u0039]\ufe0f?[\u20e3]|[\u002A\u0023]?\ufe0f?[\u20e3]/gu, '');
 }
+
+export const activityEmoji = (type) => {
+  switch (type) {
+    case 'ski alpin': return '⛷️';
+    case 'randonnée': return '🥾';
+    case 'marche': return '🥾';
+    case 'vélo': return '🚴‍♂️';
+    case 'gravel': return '🚵';
+    case 'vtt': return '🚵';
+    case 'course': return '🏃‍♂️';
+    case 'natation': return '🏊‍♂️';
+    case 'escalade': return '🧗‍♂️';
+    case 'tennis': return '🎾';
+    case 'padel': return '🎾';
+    case 'badminton': return '🏸';
+    case 'ping pong': return '🏓';
+    case 'golf': return '🏌️‍♂️';
+    default: return `[${type}]`;
+  }
+}
---
+++ src/pages/flux/index.md
@@ -0,0 +1,11 @@
+---
+title: Flux
+---
+
+![RSS logo](feed.png){.logo}
+
+Vous pouvez vous abonner aux différents flux de syndication de ce site :
+
+- Le [flux des activités](/flux/activites.xml)
+- Le [flux des récits](/flux/recits.xml)
+- Le [flux du blog](/flux/blog.xml)
