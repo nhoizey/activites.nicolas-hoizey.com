4f831c2e7f043d2c9b3fde2327759a41c5f9b72d
Nicolas Hoizey <nicolas@hoizey.com>
2025-10-12 18:55:42 +0200

    Larger shadow in traces

--- src/assets/js/route-settings.js
+++ src/assets/js/route-settings.js
@@ -3,6 +3,6 @@ export const route_settings = {
   route_width: 4,
   route_highlight_opacity: 1,
   route_highlight_width: 6,
-  route_shadow_additional_width: 3,
+  route_shadow_additional_width: 4,
   route_shadow_opacity: 0.7,
 };
