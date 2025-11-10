70488bbde49bfb79618c137da517b18ee5836ee3
Nicolas Hoizey <nicolas@hoizey.com>
2025-11-10 17:15:54 +0100

    Optimisation avec Cloudinary

---
+++ src/_data/build.js
@@ -0,0 +1,8 @@
+// biome-ignore lint/correctness/noUnusedImports: dotenv
+import { } from "dotenv/config";
+
+const thisTimestamp = new Date();
+
+export const env = process.env.ELEVENTY_RUN_MODE;
+export const timestamp = thisTimestamp;
+export const id = thisTimestamp.valueOf();
